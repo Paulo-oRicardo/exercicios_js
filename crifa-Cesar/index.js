d3edbcdfdad0f10f97e63ef7e02a2010abf5f889
Paulo-oRicardo <paulo.ricardo.prym@gmail.com>
2021-02-01 14:22:41 -0300

    Atualizado

--- crifa-Cesar/index.js
+++ crifa-Cesar/index.js
@@ -1,7 +1,7 @@
 var conteudo = prompt("Qual a conteudo a ser codificada?");
 var codificador = parseInt(prompt("Quanto será mudado?"));
 var conteudoCodificado = "";
-var conteudoDescodificado = "";
+
 
    for(i = 0 ; i < conteudo.length; i++){
       conteudoCodificada += String.fromCharCode(conteudo.charCodeAt(i) + codificador);    
@@ -9,8 +9,4 @@ var conteudoDescodificado = "";
 
 alert(conteudoCodificada); 
 
-   for(i = 0 ; i < conteudoCodificada.length; i++){       
-      conteudoDescodificada += String.fromCharCode(conteudoCodificada.charCodeAt(i) - codificador);
-   }   
-
-alert(conteudoDescodificada);
+  
